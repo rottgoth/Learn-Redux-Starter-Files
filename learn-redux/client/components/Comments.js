73bc9041456db52ad525c7baa33ee6aac1d1e599
Rob Cataneo <robcataneo@gmail.com>
2017-07-12 20:55:35 -0700

    Video 14 - Displaying and Adding Comments

--- learn-redux/client/components/Comments.js
+++ learn-redux/client/components/Comments.js
@@ -1,10 +1,29 @@
 import React from 'react';
 
 const Comments = React.createClass({
-  render() {
+  renderComment(comment, i) {
     return (
-      <div className="comment">
-        I'm the comments!!
+      <div className="comment" key={i}>
+        <p>
+          <strong>{comment.user}</strong>
+          {comment.text}
+          <button className="remove-comment">&times;</button>
+        </p>
+      </div>
+    );
+  },
+
+  render() {
+    const { postComments } = this.props;
+
+    return (
+      <div className="comments">
+        {postComments.map(this.renderComment)}
+        <form ref="commentForm" className="comment-form">
+          <input type="text" ref="author" placeholder="author" />
+          <input type="text" ref="comment" placeholder="comment" />
+          <input type="submit" hidden />
+        </form>
       </div>
     );
   }
--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -5,13 +5,15 @@ import Comments from './Comments';
 const Single = React.createClass({
   render() {
     const { posts, params } = this.props;
-    const index = posts.findIndex((post) => post.code === params.postId);
+    const { postId } = params;
+    const index = posts.findIndex((post) => post.code === postId);
     const post = posts[index];
+    const postComments = this.props.comments[postId] || [];
 
     return (
       <div className="single-photo">
         <Photo i={index} post={post} {...this.props} />
-        <Comments />
+        <Comments postComments={postComments}/>
       </div>
     );
   }
