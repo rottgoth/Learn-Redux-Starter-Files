c9baa6d49a16443561409a62408593a71d08a232
Rob Cataneo <robcataneo@gmail.com>
2017-07-12 20:45:59 -0700

    Video 13 - Displaying the Single Photo Component

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
 const Single = React.createClass({
   render() {
+    const { posts, params } = this.props;
+    const index = posts.findIndex((post) => post.code === params.postId);
+    const post = posts[index];
+
     return (
       <div className="single-photo">
-        I'm the single
+        <Photo i={index} post={post} {...this.props} />
+        <Comments />
       </div>
-    )
+    );
   }
 });
 
