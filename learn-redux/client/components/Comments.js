dff38628a34f8f468215a56d67fd0a147cb0595e
Rob Cataneo <robcataneo@gmail.com>
2017-07-13 19:53:09 -0700

    Video 16 - Redux Reducer Composition

--- learn-redux/client/components/Comments.js
+++ learn-redux/client/components/Comments.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 const Comments = React.createClass({
   renderComment(comment, i) {
+    const { postId } = this.props.params;
     return (
       <div className="comment" key={i}>
         <p>
           <strong>{comment.user}</strong>
           {comment.text}
-          <button className="remove-comment">&times;</button>
+          <button className="remove-comment" onClick={this.props.removeComment.bind(null, postId, i)}>&times;</button>
         </p>
       </div>
     );
@@ -19,6 +20,7 @@ const Comments = React.createClass({
     const author = this.refs.author.value;
     const comment = this.refs.comment.value;
     this.props.addComment(postId, author, comment);
+    this.refs.commentForm.reset();
   },
 
   render() {
--- learn-redux/client/reducers/comments.js
+++ learn-redux/client/reducers/comments.js
@@ -1,5 +1,31 @@
+function postComments(state=[], action) {
+  switch(action.type) {
+    case 'ADD_COMMENT':
+      // return the new state with the new comment
+      const comment = { user: action.author, text: action.comment};
+      return [...state, comment];
+    case 'REMOVE_COMMENT':
+      // we need to return the new state without the deleted comment
+      const { i } = action;
+      return [
+        ...state.slice(0, i),
+        ...state.slice(i + 1),
+      ];
+    default:
+      return state;
+  }  
+}
+
 function comments(state=[], action) {
-  console.log(state, action);
+  const { postId } = action;
+  if (typeof postId !== 'undefined') {
+    return {
+      // take current state
+      ...state,
+      // override this post with a new one
+      [postId]: postComments(state[postId], action)
+    }
+  }
   return state;
 }
 
