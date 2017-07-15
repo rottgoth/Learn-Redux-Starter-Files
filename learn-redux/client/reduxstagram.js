42167ca6129cdc1371526a756686dbd8e50b35ad
Rob Cataneo <robcataneo@gmail.com>
2017-07-15 09:59:55 -0700

    Video 17 - Error Tracking and Insights with Sentry

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -15,6 +15,22 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store, { history } from './store';
 
+import Raven from 'raven-js';
+import { sentry_url, logException } from './data/config';
+
+Raven.config(sentry_url, {
+  tags: {
+    git_commit: '2342acdef2',
+    userLevel: 'editor'
+  }
+}).install();
+
+// logException(new Error('download failed!'), {
+//   email: 'wesbos@gmail.com'
+// });
+// Raven.captureMessage('Something bad happened!');
+// Raven.showReportDialog()('Something bad happened!');
+
 const router = (
   <Provider store={store}>
     <Router history={history}>
